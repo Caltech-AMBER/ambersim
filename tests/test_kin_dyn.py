5001a6b09c6aedde27362267ea3bb6f943f54bdb
alberthli <alberthli@caltech.edu>
2023-11-08 22:27:45 -0800

    wrote forward kinematics test by comparing to pinocchio

--- tests/test_kin_dyn.py
+++ tests/test_kin_dyn.py
@@ -1,60 +1,76 @@
+import jax.numpy as jnp
 import mujoco as mj
 import numpy as np
 import pinocchio as pin
+import pytest
 from mujoco import mjx
 
 from ambersim import ROOT
-from ambersim.utils.io_utils import load_mjx_model_and_data_from_file
+from ambersim.utils.introspection_utils import get_joint_names
+from ambersim.utils.io_utils import load_mj_model_from_file, mj_to_mjx_model_and_data
 
 
-def test_fk():
-    """Tests forward kinematics."""
+@pytest.fixture
+def shared_variables():
+    """Configuration method for global variables."""
+    # run all tests on the barrett hand
     urdf_path = ROOT + "/models/barrett_hand/bh280.urdf"
 
-    # from ambersim.utils.io_utils import load_mj_model_from_file
+    # loading mjx models
+    mj_model = load_mj_model_from_file(urdf_path, force_float=True)
+    mjx_model, mjx_data = mj_to_mjx_model_and_data(mj_model)
+    mj_joint_names = get_joint_names(mj_model)
 
-    # mj_model = load_mj_model_from_file(urdf_path, force_float=True)
-
-    # mjx
-    # floating base state convention: (translation[x, y, z], quat[w, x, y, z])
-    mjx_model, mjx_data = load_mjx_model_and_data_from_file(urdf_path, force_float=True)
-    qpos_new = mjx_data.qpos.at[7:].set(0.5)  # set all non-quat states to 0.5
-    data_new = mjx_data.replace(qpos=qpos_new)
-    data_post_kin = mjx.kinematics(mjx_model, data_new)
-    xpos_mjx_post_kin = data_post_kin.xpos
-    print(xpos_mjx_post_kin)
-
-    # pinocchio
-    # floating base state convention: (translation[x, y, z], quat[x, y, z, w])
+    # loading pinocchio models
     pin_model = pin.buildModelFromUrdf(urdf_path)
     pin_model.addJoint(0, pin.JointModelFreeFlyer(), pin.SE3.Identity(), "freejoint")
     pin_data = pin_model.createData()
-    breakpoint()
-    qpos_quat_xyzw = np.roll(np.array(qpos_new)[3:7], -1, axis=0)
-    pin_qpos = np.concatenate((np.array(qpos_new)[7:], np.array(qpos_new)[:7]))
-    pin_qpos[-4:] = qpos_quat_xyzw
-    pin.forwardKinematics(pin_model, pin_data, pin_qpos)
-    pin.updateFramePlacements(pin_model, pin_data)
-    for oMi in pin_data.oMi:
-        print(oMi.translation)
-    breakpoint()
+    pin_joint_names = list(pin_model.names)
 
-    # # mjx
-    # # floating base state convention: (translation[x, y, z], quat[w, x, y, z])
-    # mjx_model, mjx_data = load_mjx_model_and_data_from_file(urdf_path, force_float=False)
-    # qpos_new = mjx_data.qpos.at[:].set(0.5)
-    # data_new = mjx_data.replace(qpos=qpos_new)
-    # data_post_kin = mjx.kinematics(mjx_model, data_new)
-    # xpos_mjx_post_kin = data_post_kin.xpos
+    # setting dummy positions
+    _qpos_mj = []
+    for mjjn in mj_joint_names:
+        if mjjn != "freejoint":
+            _qpos_mj.append([0.5])  # arbitrary
+        else:
+            # floating base state convention: (translation[x, y, z], quat[w, x, y, z])
+            _qpos_mj.append([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
 
-    # # pinocchio
-    # # floating base state convention: (translation[x, y, z], quat[x, y, z, w])
-    # pin_model = pin.buildModelFromUrdf(urdf_path)
-    # pin_data = pin_model.createData()
-    # pin_qpos = np.array(qpos_new)
-    # pin.forwardKinematics(pin_model, pin_data, pin_qpos)
-    # pin.updateFramePlacements(pin_model, pin_data)
-    # breakpoint()
+    _qpos_pin = []
+    for pjn in pin_joint_names:
+        if pjn not in ["freejoint", "universe"]:
+            _qpos_pin.append([0.5])  # arbitrary
+        elif pjn != "universe":
+            # floating base state convention: (translation[x, y, z], quat[x, y, z, w])
+            _qpos_pin.append([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+
+    qpos_mj = jnp.array(np.concatenate(_qpos_mj))
+    qpos_pin = np.concatenate(_qpos_pin)
+
+    # return shared variables
+    return mjx_model, mjx_data, mj_joint_names, pin_model, pin_data, pin_joint_names, qpos_mj, qpos_pin
+
+
+def test_fk(shared_variables):
+    """Tests forward kinematics.
+
+    Checks against pinocchio. The two main bookkeeping things are (1) the joint names and (2) the different quaternion convention.
+    """
+    # loading global variables
+    mjx_model, mjx_data, mj_joint_names, pin_model, pin_data, pin_joint_names, qpos_mj, qpos_pin = shared_variables
+
+    # running FK test
+    mjx_data = mjx.kinematics(mjx_model, mjx_data.replace(qpos=qpos_mj))
+    pin.forwardKinematics(pin_model, pin_data, qpos_pin)
+
+    for i, mjjn in enumerate(mj_joint_names):
+        j = pin_joint_names.index(mjjn)
+        assert np.allclose(
+            np.array(mjx_data.xanchor[i, :]),
+            np.array(pin_data.oMi[j].translation),
+            rtol=1e-6,
+            atol=1e-6,  # loosen atol a bit
+        )
 
 
 def test_fd():
