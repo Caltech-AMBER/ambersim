68c3395833fc78fdbad1d17ecda6343f12d738cf
Vince Kurtz <vjkurtz@gmail.com>
2023-11-14 12:22:14 -0800

    replace renderer with simulation

--- examples/rl/pendulum/ex_pendulum_swingup.py
+++ examples/rl/pendulum/ex_pendulum_swingup.py
@@ -1,37 +1,33 @@
 import functools
+import time
 from datetime import datetime
 
-import cv2
 import jax
-import jax.numpy as jnp
-import matplotlib.pyplot as plt
-import mujoco as mj
-import numpy as np
+import mujoco
+import mujoco.viewer
 from brax import envs
 from brax.training.agents.ppo import networks as ppo_networks
 from brax.training.agents.ppo import train as ppo
-from jax import jit
 from mujoco import mjx
 
-from ambersim.rl.base import State
 from ambersim.rl.pendulum.swingup import PendulumSwingupEnv
 
 if __name__ == "__main__":
-    # initialize the env
-    env_name = "pendulum_swingup"
+    # Initialize the environment
     envs.register_environment("pendulum_swingup", PendulumSwingupEnv)
-    env = envs.get_environment(env_name)
+    env = envs.get_environment("pendulum_swingup")
 
-    # boilerplate PPO stuff that we can probably write utils to reduce
+    # Define the training function
     make_networks_factory = functools.partial(
         ppo_networks.make_ppo_networks,
-        policy_hidden_layer_sizes=(128, 128, 128, 128),
+        policy_hidden_layer_sizes=(128,) * 4,
+        value_hidden_layer_sizes=(256,) * 5,
     )
     train_fn = functools.partial(
         ppo.train,
         num_timesteps=100_000,
-        num_evals=50,
-        reward_scaling=0.1,
+        num_evals=20,
+        reward_scaling=1.0,
         episode_length=200,
         normalize_observations=True,
         action_repeat=1,
@@ -47,14 +43,16 @@ if __name__ == "__main__":
         seed=0,
     )
 
+    # Define a callback to log progress
     times = [datetime.now()]
 
     def progress(num_steps, metrics):
         """Logs progress during RL."""
-        print(f'num_steps={num_steps}, reward={metrics["eval/episode_reward"]}')
+        print(f"  Steps: {num_steps}, Reward: {metrics['eval/episode_reward']}")
         times.append(datetime.now())
 
     # Do the training
+    print("Training...")
     make_inference_fn, params, _ = train_fn(
         environment=env,
         progress_fn=progress,
@@ -63,54 +61,31 @@ if __name__ == "__main__":
     print(f"Time to jit: {times[1] - times[0]}")
     print(f"Time to train: {times[-1] - times[1]}")
 
-    # Render a trajectory with the trained policy
-    renderer = mj.Renderer(env.model)
+    # Run an interactive simulation with the trained policy
+    mj_model = env.model
+    mj_data = mujoco.MjData(mj_model)
 
-    def get_image(state: State, camera: str) -> np.ndarray:
-        """Renders the environment state."""
-        d = mj.MjData(env.model)
-        mjx.device_get_into(d, state.pipeline_state)
-        mj.mj_forward(env.model, d)
-        renderer.update_scene(d, camera=camera)
-        return renderer.render()
-
-    inference_fn = make_inference_fn(params)
-    jit_inference_fn = jit(inference_fn)
-
-    jit_reset = jit(env.reset)
-    jit_step = jit(env.step)
+    policy = make_inference_fn(params)
+    jit_policy = jax.jit(policy)
 
     rng = jax.random.PRNGKey(0)
-    state = jit_reset(rng)
-    images = [get_image(state, camera="camera")]
+    with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
+        while viewer.is_running():
+            start_time = time.time()
+            act_rng, rng = jax.random.split(rng)
 
-    # grab a trajectory
-    n_steps = 500
-    render_every = 2
+            # Apply the policy
+            obs = env.compute_obs(mjx.device_put(mj_data), {})
+            act, _ = jit_policy(obs, act_rng)
+            mj_data.ctrl[:] = act
 
-    for i in range(n_steps):
-        act_rng, rng = jax.random.split(rng)
-        ctrl, _ = jit_inference_fn(state.obs, act_rng)
-        state = jit_step(state, ctrl)
-        if i % render_every == 0:
-            images.append(get_image(state, camera="camera"))
+            # Step the simulation
+            for _ in range(env._physics_steps_per_control_step):
+                mujoco.mj_step(mj_model, mj_data)
+                viewer.sync()
 
-        if state.done:
-            break
-
-    # Save images as video
-    output_video_file = "output_video.mp4"
-    height, width, channels = images[0].shape
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # You can try 'XVID' or 'MJPG' as well
-    video_writer = cv2.VideoWriter(output_video_file, fourcc, 10.0, (width, height))
-
-    # Iterate through the list of images and write each frame to the video file
-    for image in images:
-        # Convert the byte array to a NumPy array
-        frame = np.frombuffer(image, dtype=np.uint8).reshape(height, width, channels)
-
-        # Write the frame to the video file
-        video_writer.write(frame)
-
-    # Release the VideoWriter object
-    video_writer.release()
+            # Try to run in roughly realtime
+            elapsed = time.time() - start_time
+            dt = float(env.dt)
+            if elapsed < dt:
+                time.sleep(dt - elapsed)
