70cfc31bb43f06c7a08cfe7ef329b1d8df1ff929
Vince Kurtz <vjkurtz@gmail.com>
2023-11-22 10:35:55 -0800

    implement simple pickleable MLP

---
+++ ambersim/rl/networks.py
@@ -0,0 +1,39 @@
+from typing import Sequence
+
+import flax.linen as nn
+import jax
+import jax.numpy as np
+from brax.training import distribution, networks, types
+from brax.training.agents.ppo.networks import PPONetworks
+
+"""
+Convienience tools for defining neural networks for RL.
+"""
+
+
+class MLP(nn.Module):
+    """A simple pickle-able multi-layer perceptron.
+
+    Args:
+        layer_sizes: Sizes of all hidden layers and the output layer.
+        activate_final: Whether to apply an activation function to the output.
+        bias: Whether to use a bias in the linear layers.
+    """
+
+    layer_sizes: Sequence[int]
+    activate_final: bool = False
+    bias: bool = True
+
+    @nn.compact
+    def __call__(self, x):
+        """Forward pass through the network."""
+        # TODO(vincekurtz): consider using jax control flow here
+        for i, layer_size in enumerate(self.layer_sizes):
+            x = nn.Dense(
+                layer_size,
+                use_bias=self.bias,
+                name=f"dense_{i}",
+            )(x)
+            if i != len(self.layer_sizes) - 1 or self.activate_final:
+                x = nn.relu(x)
+        return x
