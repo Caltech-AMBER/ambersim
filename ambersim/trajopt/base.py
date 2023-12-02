1035dd4899df3745b5b117e9cce233b4fdf3ecd0
alberthli <alberthli@caltech.edu>
2023-12-02 14:53:22 -0800

    fix spacing

--- ambersim/trajopt/base.py
+++ ambersim/trajopt/base.py
@@ -11,23 +11,23 @@ class TrajectoryOptimizerParams:
     Parameters we may want to optimize should be included here.
 
     This is left completely empty to allow for maximum flexibility in the API. Some examples:
-            - A direct collocation method might have parameters for the number of collocation points, the collocation
-              scheme, and the number of optimization iterations.
-            - A shooting method might have parameters for the number of shooting points, the shooting scheme, and the number
-              of optimization iterations.
+        - A direct collocation method might have parameters for the number of collocation points, the collocation
+          scheme, and the number of optimization iterations.
+        - A shooting method might have parameters for the number of shooting points, the shooting scheme, and the number
+          of optimization iterations.
     The parameters also include initial iterates for each type of algorithm. Some examples:
-            - A direct collocation method might have initial iterates for the controls and the state trajectory.
-            - A shooting method might have initial iterates for the controls only.
+        - A direct collocation method might have initial iterates for the controls and the state trajectory.
+        - A shooting method might have initial iterates for the controls only.
 
     Parameters which we want to remain untouched by JAX transformations can be marked by pytree_node=False, e.g.,
-            ```
-            @struct.dataclass
-            class ChildParams:
-                    ...
-                    # example field
-                    example: int = struct.field(pytree_node=False)
-                    ...
-            ```
+        ```
+        @struct.dataclass
+        class ChildParams:
+            ...
+            # example field
+            example: int = struct.field(pytree_node=False)
+            ...
+        ```
     """
 
 
@@ -43,11 +43,11 @@ class TrajectoryOptimizer:
         """Optimizes a trajectory.
 
         Args:
-                params: The parameters of the trajectory optimizer.
+            params: The parameters of the trajectory optimizer.
 
         Returns:
-                xs (shape=(N + 1, nq + nv)): The optimized trajectory.
-                us (shape=(N, nu)): The optimized controls.
+            xs (shape=(N + 1, nq + nv)): The optimized trajectory.
+            us (shape=(N, nu)): The optimized controls.
         """
         # abstract dataclasses are weird, so we just make all children implement this - to be useful, they need it
         # anyway, so it isn't really a problem if an "abstract" TrajectoryOptimizer is instantiated.
